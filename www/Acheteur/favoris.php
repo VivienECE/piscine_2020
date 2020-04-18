0584d07541119bd367380725d6d732e1935256bc
DESKTOP-1V7I5C7\Vivien <vivien.detournay@edu.ece.fr>
2020-04-18 17:27:22 +0200

    Mes infos

---
+++ www/Acheteur/favoris.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ECEbay favoris</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+	<link rel="stylesheet" type="text/css" href="acheteur.css">
+	<script type="text/javascript">$(document).ready(function(){$('.header').height($(window).height());});</script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-md">
+		<a class="navbar-brand" href="#"><img src="images/logoblanc.png" width="109" height="30"></a>
+		<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+			<div class="collapse navbar-collapse" id="main-navigation">
+				 <ul class="nav navbar-nav navbar-right">
+			        <li><a class="nav-link" href="accueil.html">ACCUEIL</a></li>
+			        <li><a class="nav-link" href="categories.html">CATEGORIES</a></li>
+			        <li><a class="nav-link" href="panier.html"><img src="images/panier.png" width="20" height="20"></a></li>
+			        <li class="ici"><a  class="nav-link" href="favoris.html"><img src="images/favoris.png" width="20" height="20"></a></li>
+			        <li><a class="nav-link" href="moncompte.html">MON COMPTE</a></li>
+			     </ul>
+			</div>
+	</nav>
+
+	<div><p><br><br><br></p></div>
+
+	<div><p><h1>MES FAVORIS</h1><br><br></p></div>
+
+	<div class="container features">
+		<div class="row">
+			<div class="col-md-4 col-md-4 col-sm-12">
+				<p align="center"><img src="images/favoris.png" height="50" width="50" style="margin-left: 70px;"></p>
+				<div align="center" class="thumbnail">
+					<a href="images/piece.jpg" target="_blank" >
+					<img src="images/piece.jpg" class="img-fluid">
+					<div class="caption">
+						<p id="id">45928548</p>
+						<p id="titre">Monnaie grecque antique</p>
+						<p id="prix">250,50 €</p>
+					</div></a>
+				</div>
+				<p align="center"><a href="#"><img  src="images/croix.png" height="40" width="40"> Retirer de mes favoris !</p></a>
+			</div>
+
+			<div class="col-md-4 col-md-4 col-sm-12">
+				<p align="center"><img src="images/favoris.png" height="50" width="50" style="margin-left: 70px;"></p>
+				<div align="center" class="thumbnail">
+					<a href="images/antiquite.jpg" target="_blank" >
+					<img src="images/antiquite.jpg" class="img-fluid">
+					<div class="caption">
+						<p id="id">45928546</p>
+						<p id="titre">Collections archéologiques et objets d'Extrême-Orient</p>
+						<p id="prix">1 589 €</p>
+					</div></a>
+				</div>
+				<p align="center"><a href="#"><img  src="images/croix.png" height="40" width="40"> Retirer de mes favoris !</p></a>
+			</div>
+
+			<div class="col-md-4 col-md-4 col-sm-12">
+				<p align="center"><img src="images/favoris.png" height="50" width="50" style="margin-left: 70px;"></p>
+				<div  align="center" class="thumbnail">
+					<a href="images/montre.jpg" target="_blank" >
+					<img src="images/montre.jpg" class="img-fluid">
+					<div class="caption">
+						<p id="id">45928547</p>
+						<p id="titre">Montre KS de poche</p>
+						<p id="prix">399,99 €</p>
+					</div></a>
+				</div>
+				<p align="center"><a href="#"><img  src="images/croix.png" height="40" width="40"> Retirer de mes favoris !</p></a>
+			</div>
+		</div>
+		<div><br></div>
+		<div class="row">
+			<div class="col-md-4 col-md-4 col-sm-12">
+				<p align="center"><img src="images/favoris.png" height="50" width="50" style="margin-left: 70px;"></p>
+				<div align="center" class="thumbnail">
+					<a href="images/piece.jpg" target="_blank" >
+					<img src="images/piece.jpg" class="img-fluid">
+					<div class="caption">
+						<p id="id">45928548</p>
+						<p id="titre">Monnaie grecque antique</p>
+						<p id="prix">250,50 €</p>
+					</div>
+				</div>
+				<p align="center"><a href="#"><img  src="images/croix.png" height="40" width="40"> Retirer de mes favoris !</p></a>
+			</div>
+			
+		</div>
+	</div>
+
+	<footer class="page-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-md-8 col-sm-12">
+						<h6 class="text-uppercase font-weight-bold">Informations additionnelles</h6>
+						<p>Ce site est destiné à la vente de particuliers à particuuliers. Il est formellement interdit aux professionnels de vendre leurs produits sur notre site.</p>
+
+						<p>Nous restons à l'écoute de nos clients et sommes disponible si ils rencontrent quelconque problème.</p>
+					</div>
+
+					<div class="col-lg-4 col-md-4 col-sm-12">
+						<h6 class="text-uppercase font-weight-bold">Contact</h6>
+						<p>37, quai de Grenelle, 75015 Paris, France <br>serviceclient@ecebay.fr <br>0800 000 689 740</p>
+					</div>
+				</div>
+				<div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d'auteur: baptistevivienclemence@ecebay.fr</div>
+		</footer>
+
+</body>
+</html>
--- www/Acheteur/mesInfos.php
+++ www/Acheteur/mesInfos.php
@@ -8,7 +8,15 @@ $db_found = mysqli_select_db($db_handle, $database);
 $debug = false;
 session_start();
 $IdAcheteur=$_SESSION['IdAcheteur'];
+$IdUtilisateur= $_SESSION['IdUtilisateur'];
 $msg="";
+
+$sql= "SELECT pseudo, motdepasse
+FROM utilisateur WHERE IdUtilisateur=$IdUtilisateur";
+$result = mysqli_query($db_handle, $sql);
+while ($data = mysqli_fetch_assoc($result)){
+$Pseudo = $data['pseudo'];
+}
 
 $sql= "SELECT Nom, Prenom, Adresse, CodePostal, Pays,Telephone,TypeDeCarte, NumeroCarte, NomCarte, ExpirationCarte, CodedeSecurite, ImageProfil
 FROM acheteur WHERE IdAcheteur=$IdAcheteur";
@@ -27,24 +35,25 @@ $NomCarte = $data['NomCarte'];
 $ExpirationCarte = $data['ExpirationCarte'];
 $CodedeSecurite = $data['CodedeSecurite'];}
 
-if (isset($_POST["button"])) {
-	if($debug){echo "<br>"."button";}
-	$prix = htmlspecialchars($_POST["enchere"]);
-	$sql="SELECT * from `offreenchere` WHERE IdAcheteur=$IdAcheteur AND IdEnchere=$IdEnchere";
-	if($debug){echo "<br>".$sql;}
-	$result=mysqli_query($db_handle, $sql);
-	if (mysqli_num_rows($result) == 0)
-	{
-		$sql="INSERT INTO `offreenchere`(`IdEnchere`, `IdAcheteur`, `Prix`) VALUES ($IdEnchere,$IdAcheteur,$prix)";
-		if($debug){echo "<br>".$sql;}
-		$result=mysqli_query($db_handle, $sql);
-		$msg="Offre envoyée";
-	}else if($prix>mysqli_fetch_assoc($result)["Prix"]){
-		$sql="UPDATE `offreenchere` SET `Prix` = $prix WHERE IdAcheteur=$IdAcheteur AND IdEnchere=$IdEnchere";
-		if($debug){echo "<br>".$sql;}
-		$result=mysqli_query($db_handle, $sql);
-		$msg="Offre mise à jour";}
-	else{$msg="Veuillez surencherir";}
+//Lorsque l'on appuie sur enregistré, récupère les infos si elles sont renseigné.
+//Ajouter les modifs sql dans chaque if...
+if (isset($_POST["modification"])) { 
+		if(isset($_POST["newpseudo"])){$newpseudo = htmlspecialchars($_POST["newpseudo"]);}
+		if(isset($_POST["newmotdepasse"])){$newmotdepasse = sha1($_POST["newmotdepasse"]);}
+		if(isset($_POST["oldmotdepasse"])){$oldmotdepasse = sha1($_POST["oldmotdepasse"]);}
+		if(isset($_POST["newnom"])){$newnom = htmlspecialchars($_POST["newnom"]);}
+		if(isset($_POST["newprenom"])){$newprenom = htmlspecialchars($_POST["newprenom"]);}
+		if(isset($_POST["newadresse"])){$newadresse = htmlspecialchars($_POST["newadresse"]);}
+		if(isset($_POST["newcodepostal"])){$newcodepostal = htmlspecialchars($_POST["newcodepostal"]);}
+		if(isset($_POST["newpays"])){$newpays = htmlspecialchars($_POST["newpays"]);}
+		if(isset($_POST["newtelephone"])){$newtelephone = htmlspecialchars($_POST["newtelephone"]);}
+		//$typedecarte = htmlspecialchars($_POST["typedecarte"]);
+		if(isset($_POST["newnumerocarte"])){$newnumerocarte = htmlspecialchars($_POST["newnumerocarte"]);}
+		if(isset($_POST["newnomcarte"])){$newnomcarte= htmlspecialchars($_POST["newnomcarte"]);}
+		//$MM = htmlspecialchars($_POST["MM"]);$YY = htmlspecialchars($_POST["YY"]);
+		//$expirationcarte = $MM.'/'.$YY;
+		if(isset($_POST["newcodedesecurite"])){$newcodedesecurite = htmlspecialchars($_POST["newcodedesecurite"]);}
+		if(isset($_POST["newexpirationcarte"])){$newexpirationcarte = htmlspecialchars($_POST["newexpirationcarte"]);}
 }
 // Display the decrypted string 
 //fermer la connexion
@@ -70,11 +79,11 @@ mysqli_close($db_handle);?>
 		</button>
 			<div class="collapse navbar-collapse" id="main-navigation">
 				 <ul class="nav navbar-nav navbar-right">
-			        <li><a class="nav-link" href="accueil.html">ACCUEIL</a></li>
-			        <li><a class="nav-link" href="categories.html">CATEGORIES</a></li>
-			        <li><a class="nav-link" href="panier.html"><img src="images/panier.png" width="20" height="20"></a></li>
-			        <li><a class="nav-link" href="favoris.html"><img src="images/favoris.png" width="20" height="20"></a></li>
-			        <li class="ici"><a class="nav-link" href="moncompte.html">MON COMPTE</a></li>
+			        <li><a class="nav-link" href="accueil.php">ACCUEIL</a></li>
+			        <li><a class="nav-link" href="categories.php">CATEGORIES</a></li>
+			        <li><a class="nav-link" href="panier.php"><img src="images/panier.png" width="20" height="20"></a></li>
+			        <li><a class="nav-link" href="favoris.php"><img src="images/favoris.png" width="20" height="20"></a></li>
+			        <li class="ici"><a class="nav-link" href="moncompte.php">MON COMPTE</a></li>
 			     </ul>
 			</div>
 	</nav>
@@ -86,17 +95,34 @@ mysqli_close($db_handle);?>
 	<div class="containerINFOS">
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12" align="center">
-				<img src="images/compte.png" height="100" width="100">
-				<form>
+				<img id = "profil" src=<?php echo "'$ImageProfil'";?> height="100" width="100">
+				<form method=post>
 					<table align="center">
 						<tr align="center">
-							<td><input type="text" id="nomUtilisateur" placeholder=" VinceDup"></td>
+							<td><!--IMAGE DU PROFIL -->
+								<input type="hidden" name="MAX_FILE_SIZE" value="30000" />  <!--Apparament taille max de l'image en ko? -->
+								<input style="border-radius: 2rem;" type="file" name="newimage" accept="image/gif, image/jpeg, image/png" onchange="loadFile(event)"> 
+								<script>
+								  var loadFile = function(event) {
+								    var output = document.getElementById('profil');
+								    output.src = URL.createObjectURL(event.target.files[0]);
+								    output.onload = function() {
+								      URL.revokeObjectURL(output.src) // free memory
+								    }
+								  };
+								</script>
+								<!--IMAGE DU PROFIL --></td>
 						</tr>
 						<tr align="center">
-							<td><input type="pass" id="mdp" placeholder=" Mot de passe "></td>
+							<td><input type="text" id="nomUtilisateur" name="newpseudo" placeholder= <?php echo "'$Pseudo'";?>></td>
+						</tr>
+						<tr align="center">
+							<td><input type="password" id="mdp" name="oldmotdepasse" placeholder=" Mot de passe actuel "></td>
+						</tr>
+						<tr align="center">
+							<td><input type="password" id="mdp" name="newmotdepasse" placeholder=" Nouveau mot de passe "></td>
 						</tr>
 					</table>
-				</form method=post>
 			</div>
 		</div>
 
@@ -104,25 +130,27 @@ mysqli_close($db_handle);?>
 				<div class="col-lg-2 col-md-2 col-sm-12"></div>
 				<div class="col-lg-4 col-md-4 col-sm-12">
 					<div><p><br></p></div>
-					<form>
 						<table align="center">
 							<tr align="center">
-								<td><input type="text" id="prenom" placeholder= <?php echo "'$Prenom'";?>><input type="text" id="nom" placeholder=<?php echo "'$Nom'";?>></td>
+								<td><input type="text" id="prenom" name="newprenom" placeholder= <?php echo "'$Prenom'";?>></td>
 							</tr>
 							<tr align="center">
-								<td><input type="text" id="adresse1" placeholder=<?php echo "'$Adresse'";?>></td>
+								<td><input type="text" id="nom" name="newnom" placeholder=<?php echo "'$Nom'";?>></td>
+							</tr>
+							<tr align="center">
+								<td><input type="text" id="adresse1" name="newadresse" placeholder=<?php echo "'$Adresse'";?>></td>
 							</tr>
 							<tr align="center">
 								<td><input type="text" id="adresse2" placeholder=<?php echo "'$Prenom'";?>></td>
 							</tr>
 							<tr align="center">
-								<td><input type="number" id="codePostal" placeholder=<?php echo "'$CodePostal'";?>></td>
+								<td><input type="number" id="codePostal" name="newcodepostal" placeholder=<?php echo "'$CodePostal'";?>></td>
 							</tr>
 							<tr align="center">
-								<td><input type="text" id="pays" placeholder=<?php echo "'$Pays'";?>></td>
+								<td><input type="text" id="pays" name="newpays" placeholder=<?php echo "'$Pays'";?>></td>
 							</tr>
 							<tr align="center">
-								<td><input type="phone" id="number" placeholder=<?php echo "'$Telephone'";?>></td>
+								<td><input type="phone" id="number" name="newtelephone" placeholder=<?php echo "'$Telephone'";?>></td>
 							</tr>
 						</table>
 					
@@ -136,19 +164,18 @@ mysqli_close($db_handle);?>
 				<div class="col-lg-4 col-md-4 col-sm-12">
 					<div><p><br></p></div>
 					<p id="carte" align="center"><img src="images/visa.png"><img src="images/MC.png"><img src="images/AE.png"><img src="images/paypal.png"></p>
-					<form>
 						<table align="center">
 							<tr align="center">
-								<td><input type="text" id="numCarte"  maxlength="19" placeholder=<?php echo substr_replace($NumeroCarte, '**********', 0, -4);?>></td>
+								<td><input type="text" id="numCarte"  maxlength="19" name="newnumerocarte" placeholder=<?php echo substr_replace($NumeroCarte, '**********', 0, -4);?>></td>
 							</tr>
 							<tr align="center">
-								<td><input type="text" id="nomCarte" placeholder=<?php echo "'$NomCarte'";?>></td>
+								<td><input type="text" id="nomCarte" name="newnomcarte" placeholder=<?php echo "'$NomCarte'";?>></td>
 							</tr>
 							<tr align="center">
-								<td style="color: grey;">Date d'expiration :  <input type="text" pattern="{2}/{2}[0-9]" maxlength="5" id="dateCatre" placeholder=<?php echo "'$ExpirationCarte'";?>></td>
+								<td style="color: grey;">Date d'expiration :  <input type="text" pattern="{2}/{2}[0-9]" name="newexpirationcarte" maxlength="5" id="dateCatre" placeholder=<?php echo "'$ExpirationCarte'";?>></td>
 							</tr>
 							<tr align="center">
-								<td style="color: grey;">Code secret à 3 chiffres :  <input type="number" style="width: 60px;" id="CVV" placeholder="CVV"></td>
+								<td style="color: grey;">Code secret à 3 chiffres :  <input type="number" style="width: 60px;" id="CVV" name="newcodedesecurite" placeholder="CVV"></td>
 							</tr>
 						</table>	
 				</div>

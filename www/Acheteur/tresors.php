3bea0d371b265acb26c742c6a19fc44c7d4dcb49
DESKTOP-1V7I5C7\Vivien <vivien.detournay@edu.ece.fr>
2020-04-18 10:38:31 +0200

    Fix PHP

--- www/Acheteur/tresors.php
+++ www/Acheteur/tresors.php
@@ -7,7 +7,8 @@ $db_handle = mysqli_connect('localhost', 'root', '');
 $db_found = mysqli_select_db($db_handle, $database);
 $debug = true;
 
-$iditem=array(); $nomitem=array(); $imageitem=array(); $prixitem=array();$typeitem=array();
+$iditem=array(); $nomitem=array(); $imageitem=array(); $prixitem=array();$typeitem=array();$hrefitem=array();
+
 
 $sql= "SELECT item.IdItem, Nom, Image, PrixFinal
 FROM item
@@ -18,7 +19,8 @@ while ($data = mysqli_fetch_assoc($result)){
 array_push($iditem,$data['IdItem']);
 array_push($nomitem,$data['Nom']);
 array_push($imageitem,$data['Image']);
-array_push($prixitem,"Vente aux enchères !");}
+array_push($prixitem,"Vente aux enchères !");
+array_push($hrefitem,"clicEncheres.php");}
 
 $sql= "SELECT item.IdItem, Nom, Image, PrixFinal
 FROM item
@@ -29,7 +31,8 @@ while ($data = mysqli_fetch_assoc($result)){
 array_push($iditem,$data['IdItem']);
 array_push($nomitem,$data['Nom']);
 array_push($imageitem,$data['Image']);
-array_push($prixitem,"Proposez une offre au vendeur !");}
+array_push($prixitem,"Proposez une offre au vendeur !");
+array_push($hrefitem,"clicOffre.php");}
 
 $sql= "SELECT item.IdItem, Nom, Image, PrixFinal
 FROM item
@@ -40,13 +43,14 @@ while ($data = mysqli_fetch_assoc($result)){
 array_push($iditem,$data['IdItem']);
 array_push($nomitem,$data['Nom']);
 array_push($imageitem,$data['Image']);
-array_push($prixitem,$data['PrixFinal']."€");}
+array_push($prixitem,$data['PrixFinal']."€");
+array_push($hrefitem,"clicImmediat.php");}
 
-function display_item($iditem,$nomitem,$imageitem,$prixitem) 
+function display_item($iditem,$nomitem,$imageitem,$prixitem,$hrefitem) 
 {
 	echo "	<div class='col-md-4 col-md-4 col-sm-12'>
 				<div align='center' class='thumbnail'>
-					<a href='images/antiquite.jpg' target='_blank' ><img src=$imageitem class='img-fluid'>
+					<a href='$hrefitem"."?id=$iditem' target='_blank' ><img src=$imageitem class='img-fluid'>
 					<div class='caption'>
 						<p id='id'>$iditem</p>
 						<p id='titre'>$nomitem</p>
@@ -94,7 +98,7 @@ mysqli_close($db_handle);?>
 
 	<div class="container features">
 				<div class="row"> <!--AFFICHAGE DE TT LES ARTICLES CATEGORIE TRESOR DEPUIS LA BDD-->
-					<?php for($i = 0;$i < sizeof($iditem);$i++){display_item($iditem[$i],$nomitem[$i],$imageitem[$i],$prixitem[$i]);}?>
+					<?php for($i = 0;$i < sizeof($iditem);$i++){display_item($iditem[$i],$nomitem[$i],$imageitem[$i],$prixitem[$i],$hrefitem[$i]);}?>
 
 	<footer class="page-footer">
 			<div class="container">
